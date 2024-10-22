cf4a4d598f2e15cceabe7d72fcc5cfb1fa98b2ae
Majd A <majd.azar88@gmail.com>
2024-10-22 10:23:11 +0200

    Bug fix front end validation post form

--- src/utils/postValidation.jsx
+++ src/utils/postValidation.jsx
@@ -4,13 +4,13 @@ export const validatePost = (newPostContent, newPostTitle) => {
   const errors = {};
 
   // Validate comment content: at least 5 characters long
-  if (!newPostContent || newPostContent.trim().length < 5) {
-    errors.title = "Content must be at least 5 characters long.";
+  if (!newPostTitle || newPostTitle.trim().length < 5) {
+    errors.title = "Title must be at least 5 characters long.";
   }
 
   // Validate comment content: at least 5 characters long
-  if (!newPostTitle || newPostTitle.trim().length < 5) {
-    errors.content = "Title must be at least 5 characters long.";
+  if (!newPostContent || newPostContent.trim().length < 5) {
+    errors.content = "Content must be at least 5 characters long.";
   }
 
   // Return errors object
