ab213602a5294c0e2a5cdfa0286561f0da20b632
Majd A <majd.azar88@gmail.com>
2024-10-28 06:02:55 +0100

    Removed base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "topblogfe-production.up.railway.app", // Set this to '/' or the root URL where your app will be hosted
 });
