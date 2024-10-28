b02925aa76ac3ae42ae3cc1292f0b98a182f65c9
Majd A <majd.azar88@gmail.com>
2024-10-28 06:00:44 +0100

    Added new base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Set this to '/' or the root URL where your app will be hosted
+  base: "topblogfe-production.up.railway.app", // Set this to '/' or the root URL where your app will be hosted
 });
